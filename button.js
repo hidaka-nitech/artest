7b58747c226605d69fccbe4b8edddceffe8f207a
hidaka-nitech <155151148+hidaka-nitech@users.noreply.github.com>
2024-06-20 22:11:29 +0900

    Update button.js

--- button.js
+++ button.js
@@ -1,1 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+            const model1 = document.getElementById('model1');
+            const model2 = document.getElementById('model2');
+            const Btn = document.getElementById('');
 
+            toggleModelBtn.addEventListener('click', () => {
+            model1.setAttribute('visible', false);
+            model2.setAttribute('visible', true);
+            });
+        });
